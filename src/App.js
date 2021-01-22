35be323c37a9d9f1b3fbc5d238090a33656a2fcf
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2021-01-22 18:58:28 +0000

    works with basic adaptive sizing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import Title from './slides/slide-title';
-import Summary from './slides/slide-summary';
-import Team from './slides/slide-team';
-import Advisors from './slides/slide-advisors';
-import Problem from './slides/slide-problem';
-import Solution from './slides/slide-solution';
-import Product from './slides/slide-product';
-import Model from './slides/slide-model';
-import Market from './slides/slide-market';
-import Competitors from './slides/slide-competitors';
-import Timeline from './slides/slide-timeline';
-import Contact from './slides/slide-contact';
-
+import Content from './content';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Title/>
+      <svg viewbox="0 0 150 150" className="background">
+        <circle cx="50%" cy="50%" r="60%"/>
+      </svg>
+      <Content/>
     </div>
   );
 }
